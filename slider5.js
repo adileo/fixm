1ad1e76622a6cc7247e6104c5ff0278a101ca130
Adileo Barone <Adileob@gmail.com>
2021-10-13 18:09:17 +0200

    Update and rename slider4.js to slider5.js

--- slider5.js
+++ slider5.js
@@ -4,35 +4,10 @@ console.log('VIDEO 3 TEST')
         playerId: "default",
         videoId: "6276482491001"
     };
-    function addPlayer(el, cb) {
-        console.log('ADD PLAYER')
-        // Dynamically build the player video element
-        playerHTML =
-            '<video-js data-video-id="' +
-            playerData.videoId +
-            '"  data-account="' +
-            playerData.accountId +
-            '" data-player="' +
-            playerData.playerId +
-            '" data-embed="default" class="vjs-fluid vjs-4-3" data-muted="" loop></video-js>';
-        // Inject the player code into the DOM
-        el.innerHTML = playerHTML
-        // Add and execute the player script tag
-        var s = document.createElement("script");
-        s.src =
-            "https://players.brightcove.net/" +
-            playerData.accountId +
-            "/" +
-            playerData.playerId +
-            "_default/index.min.js";
-        // Add the script tag to the document
-        document.body.appendChild(s);
-        // Call a function to play the video once player's JavaScropt loaded
-        if (cb)
-            s.onload = cb;
-    }
+  
     var me = document.currentScript;
 console.log('ME PARENT',me.parentElement)
+
     var children = me.parentElement.children
     for (var i=0; i < children.length; i++) {
             if (!children[i].classList.contains("noremove")) {
@@ -40,6 +15,7 @@ console.log('ME PARENT',me.parentElement)
                 children[i].remove();
             }
         }
+/*
         function init(){
             if (window.bc) {
                 console.log('WINDOW BC')
@@ -59,6 +35,24 @@ console.log('ME PARENT',me.parentElement)
                 }, 3000)
             }
         }
-    addPlayer(me.parentElement, function(){
-        init();
-    })
+*/
+        playerHTML =
+            '<video-js data-video-id="' +
+            playerData.videoId +
+            '"  data-account="' +
+            playerData.accountId +
+            '" data-player="' +
+            playerData.playerId +
+            '" data-embed="default" class="vjs-fluid vjs-4-3" data-muted="" loop></video-js>';
+        // Inject the player code into the DOM
+        me.parentElement.innerHTML = playerHTML
+        // Add and execute the player script tag
+        var s = document.createElement("script");
+        s.src =
+            "https://players.brightcove.net/" +
+            playerData.accountId +
+            "/" +
+            playerData.playerId +
+            "_default/index.min.js";
+        // Add the script tag to the document
+        document.body.appendChild(s);
